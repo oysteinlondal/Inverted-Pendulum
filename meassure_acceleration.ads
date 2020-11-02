e0586bf19b92d7e93f32c4c4c02d8294ee261acb
oysteinlondal <oystein@nenett.no>
2020-11-02 22:58:32 +0100

    Added sensor read from gyroscope

---
+++ meassure_acceleration.ads
@@ -0,0 +1,9 @@
+--with Imu.Library; use Imu.Library
+
+package Meassure_Acceleration is
+    procedure Retrieve_Acceleration (Acc_X, Acc_Y : out Float);
+private
+    X_Coordinate : Float;
+    Y_Coordinate : Float;
+    Z_Coordinate : Float;
+end Meassure_Acceleration;
