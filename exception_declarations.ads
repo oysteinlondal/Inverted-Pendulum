dec304eeb5594ccc86493375cd4bbe0e361202c7
august95 <55133964+august95@users.noreply.github.com>
2020-11-09 11:50:20 +0100

    Update exception_declarations.ads

--- exception_declarations.ads
+++ exception_declarations.ads
@@ -3,7 +3,5 @@ package Exception_Declarations is
    Value_Exceed_Max        : Exception;
    Value_Exceed_Min        : Exception;
    Excecution_Time_Overun  : Exception;
-   Unknown_Error           : Exception;
-   Recovery_Block_Overload : Exception;
     --  Like an object. *NOT* a type !
 end Exception_Declarations;
