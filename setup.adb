80fc67804616a6b91ab48e6b22ef03df64f4de3e
Vilduha <42605310+Vilduha@users.noreply.github.com>
2020-11-09 11:56:07 +0100

    Update setup.adb
    Added digitalWrite to setup

--- setup.adb
+++ setup.adb
@@ -4,8 +4,18 @@ package body Setup is
         procedure Calibrate_Motors_If_Required is
         begin
             if not Setup_Done then
+                --Calibrating left and right ESC
                 Put("Start Setup");
-                delay 4.0;
+                Write(3,1000);
+                Set_Analog_Period_Us(20000);
+                Write(28,1000);
+                Set_Analog_Period_Us(20000);
+                NRF52_DK.Time.Delay_Ms (2000);
+                Write(3,2000);
+                Set_Analog_Period_Us(20000);
+                Write(28,2000);
+                Set_Analog_Period_Us(20000);
+                NRF52_DK.Time.Delay_Ms (2000);
                 Setup_Done := True;
             end if;
         end Calibrate_Motors_If_Required;
