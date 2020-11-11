5c86b36218400f0d3767733652e87fc7bf221db0
august95 <55133964+august95@users.noreply.github.com>
2020-11-11 20:29:04 +0100

    Update meassure_acceleration.adb

--- meassure_acceleration.adb
+++ meassure_acceleration.adb
@@ -1,9 +1,10 @@
 package body Meassure_Acceleration is
 	procedure Retrieve_Acceleration (Acc_X, Acc_y : out Float) is
     begin
-		--Imu.gyro.read(data); 
-		--z_gyro = gyro.data(z_angle);
-		--return z_gyro;
+   		--if Imu.Acc.Read.Available() not true then
+		--   raise Acc_Not_Available ;
+		--end if;
+		--Imu.acc.read(data); 
         X_Coordinate := 9.8;
         Y_Coordinate := 0.0;
         Acc_X        := X_Coordinate;
