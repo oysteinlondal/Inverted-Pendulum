9a1c8e78a41cbb014ade48fb61d9edc173bd2767
august95 <55133964+august95@users.noreply.github.com>
2020-11-11 20:27:21 +0100

    Update meassure_velocity.adb

--- meassure_velocity.adb
+++ meassure_velocity.adb
@@ -1,6 +1,9 @@
 package body Meassure_Velocity is
 	procedure Retrieve_Velocity (Velocity_Z : out Float) is
     begin
+		--if Imu.Gyro.Read.Available() not true then
+		--   raise Gyro_Not_Available ;
+		--end if;
 		--Imu.gyro.read(data); 
 		--z_gyro = gyro.data(z_angle);
 		--return z_gyro;
