a6504eecf9fbaafc5c88285ce41b7e31037a7eaa
august95 <55133964+august95@users.noreply.github.com>
2020-11-12 11:28:38 +0100

    Update exception_declarations.ads

--- exception_declarations.ads
+++ exception_declarations.ads
@@ -3,7 +3,7 @@ package Exception_Declarations is
    Value_Exceed_Max        : Exception;
    Value_Exceed_Min        : Exception;
    Excecution_Time_Overun  : Exception;
-   Gyro_Not_Available      : Exception;
-   Acc_Not_Available       : Exception;
+  -- Gyro_Not_Available      : Exception;
+   --Acc_Not_Available       : Exception;
     --  Like an object. *NOT* a type !
 end Exception_Declarations;
